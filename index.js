bf2272cd2629846d3c17fdffe65c92c18575eede
wira <wirasyafri1998@gmail.com>
2019-10-08 16:08:14 +0700

    belajar javascript

---
+++ index.js
@@ -0,0 +1,12 @@
+var message ="hello coding";
+let msg1 = "hello";
+const msg2 = "coding";
+
+if(true){
+	var message= "haha";
+	let  msg1 ="huhu";
+	console.log("huwwe",msg1);
+}
+console.log(message)
+
+console.log(msg1)
---
+++ tugas1.js
@@ -0,0 +1,5 @@
+var n_depan ="syafri";
+let n_blkg	="wira wicaksana";
+const umur =21;
+
+console.log(n_depan,n_blkg,umur)
